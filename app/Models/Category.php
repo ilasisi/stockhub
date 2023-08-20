fb405c3b1840d1046b016a649207564c8a76b2a4
Ibrahim Lasisi <ilasisi90@gmail.com>
2023-08-20 22:07:20 +0100

    sftdelete

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,10 +8,11 @@ use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use HasFactory, HasUuids;
+    use HasFactory, HasUuids, SoftDeletes;
 
     protected $guarded = ['id'];
 
